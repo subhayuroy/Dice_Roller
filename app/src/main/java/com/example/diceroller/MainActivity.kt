0a2a76587fa8b8b46fdfa96959cfa621d0301bc1
subhayuroy <subhayu.roy2001@gmail.com>
2020-08-11 14:04:50 +0530

    Making the Button interactive

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,6 +15,8 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +24,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+//          Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+        }
     }
 }
