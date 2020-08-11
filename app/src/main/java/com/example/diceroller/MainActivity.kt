7058bfc3fa5c18dcbf0f935a23c57b32d7ae07d2
subhayuroy <subhayu.roy2001@gmail.com>
2020-08-11 13:34:16 +0530

    Activities

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,26 @@
+//package com.example.diceroller;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
